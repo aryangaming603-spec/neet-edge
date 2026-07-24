c9495b9d4521333d10e4ad7887750476694bd8d4
aryangaming603-spec <aryangaming603@gmail.com>
2026-07-24 22:13:59 +0530

    Update script.js

--- script.js
+++ script.js
@@ -35,3 +35,20 @@ function updateDateTime() {
 
 updateDateTime();
 setInterval(updateDateTime, 1000);
+
+
+function updateCountdown() {
+
+    const examDate = new Date("May 2, 2027 09:00:00");
+
+    const today = new Date();
+
+    const diff = examDate - today;
+
+    const days = Math.ceil(diff / (1000 * 60 * 60 * 24));
+
+    document.getElementById("countdown").textContent =
+        days + " Days Left";
+}
+
+updateCountdown();
