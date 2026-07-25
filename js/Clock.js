6388c14eb49f579eade8205724df0037727c34a4
aryangaming603-spec <aryangaming603@gmail.com>
2026-07-25 09:58:06 +0530

    Update Clock.js

--- js/Clock.js
+++ js/Clock.js
@@ -1,1 +1,24 @@
+function updateClock() {
 
+    const now = new Date();
+
+    document.getElementById("time").textContent =
+        now.toLocaleTimeString("en-IN", {
+            hour: "2-digit",
+            minute: "2-digit",
+            second: "2-digit",
+            hour12: true
+        });
+
+    document.getElementById("date").textContent =
+        now.toLocaleDateString("en-IN", {
+            weekday: "long",
+            day: "numeric",
+            month: "long",
+            year: "numeric"
+        });
+}
+
+updateClock();
+
+setInterval(updateClock,1000);
