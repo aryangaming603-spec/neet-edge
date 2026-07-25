06c8f829e34d285d90970838e4034c202ef7fbcf
aryangaming603-spec <aryangaming603@gmail.com>
2026-07-25 10:11:20 +0530

    Update storage.js

--- js/storage.js
+++ js/storage.js
@@ -1,1 +1,32 @@
+// ==========================
+// Local Storage
+// ==========================
 
+function saveChapterProgress(){
+
+    localStorage.setItem(
+        "chapterProgress",
+        JSON.stringify(chapterProgress)
+    );
+
+}
+
+function loadChapterProgress(){
+
+    const data = JSON.parse(
+        localStorage.getItem("chapterProgress")
+    );
+
+    if(data){
+
+        chapterProgress.physics = data.physics;
+        chapterProgress.chemistry = data.chemistry;
+        chapterProgress.biology = data.biology;
+
+        updateChapterTracker();
+
+    }
+
+}
+
+loadChapterProgress();
