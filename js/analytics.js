6aca9cb680e1d4ed383d0b1452b2ec18ddaab78a
aryangaming603-spec <aryangaming603@gmail.com>
2026-07-25 10:12:18 +0530

    Update analytics.js

--- js/analytics.js
+++ js/analytics.js
@@ -1,1 +1,40 @@
+// ==========================
+// Analytics
+// ==========================
 
+function updateAnalytics(){
+
+    const completed =
+        document.querySelectorAll(
+            "#taskList input:checked"
+        ).length;
+
+    const total =
+        document.querySelectorAll(
+            "#taskList input"
+        ).length;
+
+    const analytics =
+        document.querySelector(".analytics-card");
+
+    analytics.innerHTML = `
+
+<h3>📊 Analytics</h3>
+
+<p>Completed Tasks : ${completed}</p>
+
+<p>Total Tasks : ${total}</p>
+
+<p>Completion : ${
+total>0
+?
+Math.round(completed/total*100)
+:
+0
+}%</p>
+
+`;
+
+}
+
+setInterval(updateAnalytics,500);
