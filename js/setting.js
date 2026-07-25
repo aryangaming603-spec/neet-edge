a4eb568efd6c0225db2f83128b4386efec0f0486
aryangaming603-spec <aryangaming603@gmail.com>
2026-07-25 10:19:02 +0530

    Update setting.js

--- js/setting.js
+++ js/setting.js
@@ -1,1 +1,38 @@
+// ==========================
+// Settings & Theme
+// ==========================
 
+const themeBtn = document.getElementById("themeToggle");
+
+function loadTheme() {
+
+    const savedTheme = localStorage.getItem("theme");
+
+    if (savedTheme === "light") {
+        document.body.classList.add("light-theme");
+        themeBtn.textContent = "🌞 Light";
+    }
+
+}
+
+function toggleTheme() {
+
+    document.body.classList.toggle("light-theme");
+
+    if (document.body.classList.contains("light-theme")) {
+
+        localStorage.setItem("theme", "light");
+        themeBtn.textContent = "🌞 Light";
+
+    } else {
+
+        localStorage.setItem("theme", "dark");
+        themeBtn.textContent = "🌙 Dark";
+
+    }
+
+}
+
+themeBtn.addEventListener("click", toggleTheme);
+
+loadTheme();
