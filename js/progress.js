1abb377bb7b6d4542099dc3d58e82504b669402b
aryangaming603-spec <aryangaming603@gmail.com>
2026-07-25 10:08:35 +0530

    Update progress.js

--- js/progress.js
+++ js/progress.js
@@ -1,1 +1,24 @@
+// ==========================
+// Progress System
+// ==========================
 
+const progressFill = document.querySelector(".fill");
+const progressText = document.getElementById("progressText");
+
+function updateProgress() {
+
+    const totalTasks = document.querySelectorAll("#taskList input[type='checkbox']").length;
+    const completedTasks = document.querySelectorAll("#taskList input[type='checkbox']:checked").length;
+
+    let percentage = 0;
+
+    if (totalTasks > 0) {
+        percentage = Math.round((completedTasks / totalTasks) * 100);
+    }
+
+    progressFill.style.width = percentage + "%";
+    progressText.textContent = percentage + "% Completed";
+
+}
+
+setInterval(updateProgress, 500);
