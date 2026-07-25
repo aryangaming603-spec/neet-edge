1acf38c9388d2a32005e527cd6138be505b1226f
aryangaming603-spec <aryangaming603@gmail.com>
2026-07-25 10:10:05 +0530

    Update chapter.js

--- js/chapter.js
+++ js/chapter.js
@@ -1,1 +1,26 @@
+// ==========================
+// Chapter Progress
+// ==========================
 
+const chapterProgress = {
+
+    physics:25,
+    chemistry:40,
+    biology:55
+
+};
+
+function updateChapterTracker(){
+
+    document.querySelector(".physics").style.width =
+        chapterProgress.physics + "%";
+
+    document.querySelector(".chemistry").style.width =
+        chapterProgress.chemistry + "%";
+
+    document.querySelector(".biology").style.width =
+        chapterProgress.biology + "%";
+
+}
+
+updateChapterTracker();
