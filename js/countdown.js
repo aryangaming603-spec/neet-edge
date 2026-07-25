9bc557331afaec1969e91e1214902a211df9c81a
aryangaming603-spec <aryangaming603@gmail.com>
2026-07-25 10:00:36 +0530

    Update countdown.js

--- js/countdown.js
+++ js/countdown.js
@@ -1,1 +1,17 @@
+function updateCountdown(){
 
+    // Update this when official NEET 2027 date is announced
+    const examDate = new Date("2027-05-02T09:00:00");
+
+    const today = new Date();
+
+    const diff = examDate - today;
+
+    const days = Math.ceil(diff/(1000*60*60*24));
+
+    document.getElementById("countdown").textContent =
+        days + " Days Left";
+
+}
+
+updateCountdown();
