0c9bd3341ddf216eeca809b5a90bfe043b9c5e21
aryangaming603-spec <aryangaming603@gmail.com>
2026-07-25 09:59:15 +0530

    Update greeting.js

--- js/greeting.js
+++ js/greeting.js
@@ -1,1 +1,29 @@
+function updateGreeting(){
 
+    const hour = new Date().getHours();
+
+    let greeting = "Good Night 🌙";
+
+    if(hour>=5 && hour<12){
+
+        greeting="Good Morning ☀️";
+
+    }
+
+    else if(hour>=12 && hour<17){
+
+        greeting="Good Afternoon 🌤️";
+
+    }
+
+    else if(hour>=17 && hour<21){
+
+        greeting="Good Evening 🌇";
+
+    }
+
+    document.getElementById("greeting").textContent = greeting;
+
+}
+
+updateGreeting();
